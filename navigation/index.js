4144a4bcff8b97a7dbb4c40fd23bc0fae1b1a777
Kailey <kalyhart@iu.edu>
2023-10-01 12:01:07 -0400

    Setting up delete account screen #16

--- navigation/index.js
+++ navigation/index.js
@@ -9,8 +9,9 @@ import { createStackNavigator } from "@react-navigation/stack";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 // import { Text, View } from "../components/Themed";
 import Ionicons from "@expo/vector-icons/Ionicons";
-import { onAuthStateChanged } from "firebase/auth";
+
 import { firebase_auth } from "../FirebaseConfig";
+import { onAuthStateChanged } from "firebase/auth";
 
 // * Auth Screens
 import SplashScreen from "../screens/SplashScreen";
