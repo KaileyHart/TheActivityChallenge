d57376d805fe926b920a54460eb59231af74c511
Kailey <kalyhart@iu.edu>
2023-12-10 10:43:07 -0500

    Force light mode

--- App.js
+++ App.js
@@ -2,12 +2,17 @@ import "react-native-gesture-handler";
 import { StatusBar } from "expo-status-bar";
 import { SafeAreaProvider } from "react-native-safe-area-context";
 import Navigation from "./navigation";
-import { useColorScheme } from "react-native";
+import { useColorScheme, setColorScheme } from "react-native";
 
 
 export default function App() {
 
   const colorScheme = useColorScheme();
+
+  // * Force light mode. 
+  if (colorScheme === 'dark') {
+    setColorScheme('light');
+  };
 
   return (
     <SafeAreaProvider>
